2b05dcdbf49b1962d892743b3dfd098804f5add0
demetriusleonardobantim <demetriusleonardobantim98@hotmail.com>
2021-02-16 11:14:04 -0300

    ensure LoginRouter return 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.passoword) {
       return {
         statusCode: 400,
       };
@@ -14,6 +14,16 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'any_password',
+      },
+    };
+    const httpResponse = await sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+  test('Should return 400 if no password is provider', async () => {
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
       },
     };
     const httpResponse = await sut.route(httpRequest);
