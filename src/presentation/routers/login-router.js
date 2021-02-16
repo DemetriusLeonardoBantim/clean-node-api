2c4062e95501bb469381edaf97a980fda77dcb00
demetriusleonardobantim <demetriusleonardobantim98@hotmail.com>
2021-02-16 14:29:13 -0300

    move error 401 to a helper method

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -4,20 +4,23 @@ module.exports = class LoginRouter {
   constructor(authUseCase) {
     this.authUseCase = authUseCase;
   }
+
   route(httpRequest) {
     if (!httpRequest || !httpRequest.body) {
       return HttpResponse.serverError();
     }
+
     const { email, password } = httpRequest.body;
+
     if (!email) {
       return HttpResponse.badRequest('email');
     }
+
     if (!password) {
       return HttpResponse.badRequest('password');
     }
+
     this.authUseCase.auth(email, password);
-    return {
-      statusCode: 401,
-    };
+    return HttpResponse.unauthorizedError();
   }
 };
