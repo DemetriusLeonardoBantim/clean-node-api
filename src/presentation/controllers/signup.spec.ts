2765984f4c68c40a4867f03acf5d613d403dccbf
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-26 11:16:44 -0300

    refactor: update test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -108,18 +108,17 @@ describe('SignUp Controller', () => {
     test('Should call EmailValidator with correct email', () => {
         // System under test
         const { sut, emailValidatorStub } = makeSut()
-        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
         const httpRequest = {
             body: {
                 name: 'any_name',
-                email: 'invalid_email@mail.com',
+                email: 'any_email@mail.com',
                 password: 'any_password',
                 passwordConfirmation: 'any_password'
             }
         }
 
-        const httpResponse = sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+        sut.handle(httpRequest)
+        expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
     })
 }) 
