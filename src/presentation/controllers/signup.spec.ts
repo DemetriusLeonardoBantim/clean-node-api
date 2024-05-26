ed5aa999b6f31137744a3c428eb9976e4a8f01e5
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-05-26 11:23:17 -0300

    feat: use serverError in test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { EmailValidator } from '../protocols/email-validator'
+import { ServerError } from '../errors/server-error'
 
 interface SutTypes {
     sut: SignUpController
@@ -131,18 +132,17 @@ describe('SignUp Controller', () => {
         const emailValidatorStub = new EmailValidatorStub()
         const sut = new SignUpController(emailValidatorStub)
 
-        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
         const httpRequest = {
             body: {
                 name: 'any_name',
-                email: 'invalid_email@mail.com',
+                email: 'any_email@mail.com',
                 password: 'any_password',
                 passwordConfirmation: 'any_password'
             }
         }
 
         const httpResponse = sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError())
     })
 }) 
