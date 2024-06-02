312820dce87b41dba2359b2a121ce2a07191f23d
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-02 11:11:48 -0300

    refactor: use MongoHelper

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,15 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+
 describe('Account Mongo Repository', () => {
+
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
     test('Should return an account on success', () => {
         const sut = new AccountMongoRepository()
         const account = sut.add({
