fb2eddaecf9201c613a321eb13fa1e74be2bd378
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-02 12:39:27 -0300

    refactor: refactor tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,27 +1,25 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
 
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL)
+        console.log(process.env.MONGO_URL)
     })
 
     afterAll(async () => {
         await MongoHelper.disconnect()
     })
 
-    test('Should return an account on success', () => {
+    test('Should return an account on success', async () => {
         const sut = new AccountMongoRepository()
-        const account = sut.add({
+        const account = await sut.add({
             name: 'any_name',
             email: 'any_email@mail.com',
             password: 'any_password'
         })
-
         expect(account).toBeTruthy()
-        expect(account.id).toBeTruthy()
-        expect(account.name).toEqual({ name: 'any_name' })
-        expect(account.email).toEqual({ name: 'any_email@mail.com' })
-        expect(account.password).toEqual({ name: 'any_password' })
+        expect(account.insertedId).toBeTruthy()
     })
 })
