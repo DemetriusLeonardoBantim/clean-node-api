d53ada2472bbeab5beca2af1f14c9100c43d2d65
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-15 15:49:26 -0300

    chore: add midleware setup

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setupMidlewares from './middlewares'
+import setupRoutes from '../config/routes'
 
 const app = express()
 setupMidlewares(app)
+setupRoutes(app)
 export default app
