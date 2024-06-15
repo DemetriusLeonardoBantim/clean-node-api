28bd015ec7df98991a8d0ecc31b868edc9371a38
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-15 15:49:46 -0300

    refactor: app use

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,8 @@
 import { Express } from 'express'
-import { bodyParser, contentType, cors } from '../midlewares'
+import { bodyParser } from '../midlewares/body-parser'
+import { contentType } from '../midlewares/content-type'
+import { cors } from '../midlewares/cors'
+import routes from './routes'
 
 export default (app: Express): void => {
     app.use(bodyParser)
