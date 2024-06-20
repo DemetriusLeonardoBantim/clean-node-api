f8050e82cf770e9c6a567a9c96ff5532ebfe7d54
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-20 08:19:45 -0300

    feat: create class LogControllerDecorator

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,13 +3,16 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makesignUpController = (): SignUpController => {
+
+export const makesignUpController = (): Controller => {
     const salt = 12
     const emailValidatopAdapter = new EmailValidatorAdapter()
     const accountMongoRepository = new AccountMongoRepository()
     const bcryptAdapter = new BcryptAdapter(salt)
     const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-
-    return new SignUpController(emailValidatopAdapter, dbAddAccount)
+    const signUpController = new SignUpController(emailValidatopAdapter, dbAddAccount)
+    return new LogControllerDecorator(signUpController)
 }
