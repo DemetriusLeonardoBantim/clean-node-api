bfb6e8bddee43ad9522ab81e2915d124c3f83052
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-06-26 07:51:37 -0300

    refactor: make serverError retyrb the error stack

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -11,7 +11,7 @@ export const badRequest = (error: Error): HttpResponse => {
 export const serverError = (error: Error): HttpResponse => {
     return {
         statusCode: 500,
-        body: new ServerError()
+        body: new ServerError(error.stack)
     }
 }
 
